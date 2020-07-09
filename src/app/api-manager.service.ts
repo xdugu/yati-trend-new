05eba51300bb82331c36f54ef804e1e9c30bd08b
xdugu <xdugu2007@gmail.com>
2020-07-09 22:15:24 +0200

    change api endpoint to stable version

--- src/app/api-manager.service.ts
+++ src/app/api-manager.service.ts
@@ -16,7 +16,7 @@ export enum API_METHOD {
   providedIn: 'root'
 })
 export class ApiManagerService {
-  private endPoint = "https://h0jg4s8gpa.execute-api.eu-central-1.amazonaws.com/test";
+  private endPoint = "https://h0jg4s8gpa.execute-api.eu-central-1.amazonaws.com/v1";
 
 
   constructor(private http: HttpClient) { }
